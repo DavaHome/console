00ee33dac7bb5fcc64b1d4d392f638d54a20378d
thedava <thedava@users.noreply.github.com>
2017-05-24 16:18:01 +0200

    Fixed namespace

--- src/DavaHome/Console/Command/AbstractCommand.php
+++ src/DavaHome/Console/Command/AbstractCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace DavaHome\Command\Console;
+namespace DavaHome\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\QuestionHelper;
